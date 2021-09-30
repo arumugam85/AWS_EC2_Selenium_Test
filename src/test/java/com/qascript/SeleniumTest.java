e52fb7c234b202e793f720d915859eefcbbbed22
arumugam85 <rarumugambe@gmail.com>
2021-09-30 13:35:54 -0400

    Test for aws ec2 jenkins updated

--- src/test/java/com/qascript/SeleniumTest.java
+++ src/test/java/com/qascript/SeleniumTest.java
@@ -4,45 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class SeleniumDemo {
+public class SeleniumTest {
 
-	@Parameters("Browser")
 	@Test
-	public void OpenBrowser(String browser) throws InterruptedException {
-
-		System.out.println("Parameter Name is " + browser);
-		WebDriver driver = null;
-		
-		if (browser.contains("Chrome")) {
-			ChromeOptions chromeOptions = new ChromeOptions();
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver(chromeOptions);
-			System.out.println("Launch Chrome Browser");
-		} else if (browser.contains("FireFox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-			System.out.println("Launch FF Browser");
-		}
-
-		else if (browser.contains("Edge")) {
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-			System.out.println("Launch Edge Browser");
-		}
-
+	public void firstTest() throws InterruptedException {
+		WebDriver driver;
+		// System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		ChromeOptions options = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver(options);
+		options.addArguments("headless");
+		options.addArguments("disable-gpu");
+		driver = new ChromeDriver(options);
+		System.out.println("Launch Chrome Browser");
 		driver.manage().window().maximize();
 		System.out.println("************Launch OHRM Application**************");
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Thread.sleep(3000);
-		
 		System.out.println("************Enter User name************");
 		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
 		System.out.println("**********Enter password*********");
@@ -53,7 +36,7 @@ public class SeleniumDemo {
 		System.out.println("*********Title of the page is: " + driver.getTitle());
 		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
 		driver.quit();
-		System.out.println("********Browser: "+browser+" closed**********");
+		
 	}
 
 }
